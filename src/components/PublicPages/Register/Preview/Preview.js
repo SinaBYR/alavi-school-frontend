2a380d12401c28878ccfd7565dd8375a9a59274a
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-07-30 22:26:16 +0430

    Add Preview-component to register page (progress bar desktop design)

---
+++ src/components/PublicPages/Register/Preview/Preview.js
@@ -0,0 +1,58 @@
+import classes from './Preview.module.css';
+import { BsArrowLeft } from 'react-icons/bs';
+import { useRef } from 'react';
+
+const Preview = props => {
+
+    return (
+        <div className={classes.Preview}>
+            <ul className={classes.Process}>
+
+                <li className={[classes.Phase, classes.Active].join(' ')}>
+                    شروع
+                </li>
+                <li className={[classes.Phase, classes.Active].join(' ')}>
+                    معاونت
+                </li>
+                <li className={[classes.Phase, classes.Active].join(' ')}>
+                    مشاوره
+                </li>
+                <li className={[classes.Phase].join(' ')}>
+                    ارزیابی
+                </li>
+                <li className={classes.Phase}>
+                    مدیریت
+                </li>
+                <li className={classes.Phase}>
+                    حسابداری
+                </li>
+                <li className={classes.Phase}>
+                    تکمیل پرونده
+                </li>
+                <li className={classes.Phase}>
+                    انتخاب کلاس
+                </li>
+                <li className={classes.Phase}>
+                    پایان
+                </li>
+
+            </ul>
+            <div className={classes.Content}>
+                <h2>مراحل پیش ثبت نام</h2>
+                <p>1 - پر کردن دقیق فرم های : مشخصات ، پزشکی و ورزشی ، ارائه رضایت نامه ، مطالعه آئین نامه</p>
+                <p>2 - مراجعه به معاونت جهت دریافت و امضای فرمهای ثبت نام در زمان اعلام از طرف مدرسه</p>
+                <p>3 - مراجعه به مشاور</p>
+                <p>4 - مراجعه بخش ارزیابی</p>
+                <p>5 - مراجعه به مدیریت</p>
+                <p>6 - مراجعه به حسابداری</p>
+                <p>7 - بررسی تکمیل مدارک</p>
+                <p>8 - انتخاب کلاس ( بعد از طی مراحل و تائید نهایی توسط مدیر مدرسه، منتظر شروع انتخاب کلاس باشید )</p>
+                <p>9 - پایان</p>
+                <p className={classes.Warning}><span>تذکر مهم: </span>  
+                خواهشمند است در زمان وارد کردن اطلاعات، دقت کافی داشته باشید، بدیهی است عواقب ورود اشتباه اطلاعات بر عهده ثبت نام کننده می باشد.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Preview;
--- src/components/PublicPages/Register/Register.js
+++ src/components/PublicPages/Register/Register.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import InitialStage from './InitialStage/InitialStage';
 import classes from './Register.module.css';
+import { useState } from 'react';
 import ButtonOne from '../../Utility/UI/ButtonOne/ButtonOne';
+import InitialStage from './InitialStage/InitialStage';
+import Preview from './Preview/Preview';
 
 const Register = props => {
     const [stage, setStage] = useState(0);
@@ -10,8 +11,18 @@ const Register = props => {
         <div className={classes.Register}>
             <div className={classes.Wrapper}>
                 {stage === 0 && <InitialStage />}
+                {stage === 1 && <Preview />}
                 <div className={classes.Controls}>
-                    <ButtonOne onClick={() => setStage(stage + 1)}>شروع ثبت نام</ButtonOne>
+                    <ButtonOne onClick={() => setStage(stage + 1)}>
+                        {stage === 0 && 'مرحله بعد'}
+                        {stage === 1 && 'مرحله بعد'}
+                    </ButtonOne>
+                    {
+                    stage > 0 &&
+                        <ButtonOne onClick={() => setStage(stage - 1)}>
+                            {stage === 1 && 'مرحله قبل'}
+                        </ButtonOne>
+                    }
                 </div>
             </div>
         </div>
