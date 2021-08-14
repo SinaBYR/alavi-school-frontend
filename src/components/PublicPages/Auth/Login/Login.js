845390cfcec22b8655eaf492b78ede5ba173ca87
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-08-14 19:14:09 +0430

    Refactor Input utility component + Login component inputs

--- src/components/PublicPages/Auth/Login/Login.js
+++ src/components/PublicPages/Auth/Login/Login.js
@@ -3,50 +3,48 @@ import { MdAccountBox } from 'react-icons/md';
 import ButtonOne from '../../../Utility/UI/ButtonOne/ButtonOne';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { useState } from 'react';
+import { withFormik, Form } from 'formik';
+import * as yup from 'yup';
+import { Input } from '../../../Utility/Inputs';
+import HThree from '../../../Utility/UI/Headings/HThree/HThree';
 
-const Login = props => {
+const Login = ({errors, touched}) => {
 
     const [passVisible, setPassVisible] = useState(false);
 
     return(
         <div className={classes.Login}>
             <div className={classes.Wrapper}>
-                <h2>ورود به پنل کاربری</h2>
-                <p></p>
-                <form className={classes.Form}>
+                <h2 style={{textAlign: 'center', marginBottom: '1rem'}}>ورود به پنل کاربری</h2>
+                <Form className={classes.Form}>
+                    <Input label="کدملی" type="text" name="code" error={touched.code && errors.code}/>
+                    <Input label="رمزعبور" type="password" name="password" error={touched.password && errors.password}/>
 
-                    <div className={classes.Item}>
-                        {/* <div className={classes.Icon}>
-                            <MdAccountBox />
-                        </div> */}
-                        <div className={classes.InputWrapper}>
-                            <label>کدملی</label>
-                            <input type="text" className={classes.Input}/>
-                        </div>
-                    </div>
-
-                    <div className={classes.Item}>
-                        {/* <div className={classes.Icon}>
-                            <MdAccountBox />
-                        </div> */}
-                        <div className={classes.InputWrapper}>
-                            <label>رمزعبور</label>
-                            <input type={passVisible ? 'text' : 'password'} className={classes.Input}/>
-                            <button className={classes.PasswordVisibility} onClick={() => setPassVisible(!passVisible)} type="button">
-                                {
-                                    passVisible ? <AiFillEye /> : <AiFillEyeInvisible />
-                                }
-                            </button>
-                        </div>
-                    </div>
-                    {/* <button>ورود</button> */}
                     <ButtonOne style={{width: '100%', marginTop: '2rem'}}>ورود</ButtonOne>
                     <p style={{color: 'gray', fontSize: '0.9rem', margin: '1rem 0', textAlign: 'center'}}>رمز عبور خود را فراموش کرده اید؟</p>
                     <p style={{textAlign: 'center'}}>رمزعبور اولیه، همان کدملی دانش آموز است.</p>
-                </form>
+                </Form>
             </div>
         </div>
     )
 }
 
-export default Login;
+// Minimum eight characters, at least one letter and one number:
+const passwordRegex = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+
+const options = {
+    mapPropsToValues(values) {
+        return {
+            code: '',
+            password: ''
+        }
+    },
+    validationSchema: yup.object().shape({
+        code: yup.number().required('تکمیل این فیلد الزامی است'),
+        password: yup.string().required('تکمیل این فیلد الزامی است')
+    })
+}
+
+const FormikLogin = withFormik(options)(Login);
+
+export default FormikLogin;
--- src/components/Utility/Inputs/Input/Input.js
+++ src/components/Utility/Inputs/Input/Input.js
@@ -1,13 +1,57 @@
 import classes from './Input.module.css';
-import { FastField } from "formik";
-import React from 'react';
+import { FastField, Field } from "formik";
+import React, { useEffect, useState } from 'react';
 import { BiError } from 'react-icons/bi';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 
-export const Input = ({ label, config, style, error}) => {
+export const Input = ({ label, inputConfig, labelConfig, parentConfig, config, style, error, type, name, accept}) => {
+
+    const [passVisible, setPassVisible] = useState(false);
+    let element;
+
+        switch (type) {
+            case 'text': {
+                element = <FastField type="text" name={name} {...inputConfig} style={{borderColor: error && 'red'}} />
+                break;
+            }
+            case 'email': {
+                element = <FastField type="email" name={name} {...inputConfig} style={{borderColor: error && 'red'}} />
+                break;
+            }
+            case 'password': {
+                element = (
+                    <>
+                        <Field type={passVisible ? 'text' : 'password'} name={name} {...inputConfig} style={{borderColor: error && 'red'}} />
+                        <button className={classes.PasswordVisibility} onClick={() => setPassVisible(!passVisible)} type="button">
+                            {
+                                passVisible ? <AiFillEye /> : <AiFillEyeInvisible />
+                            }
+                        </button>
+                    </>
+                )
+                break;
+            }
+            case 'file': {
+                element = <input type="file" name={name} accept={accept} {...inputConfig}/>
+                break;
+            }
+            case 'textarea': {
+                element = <FastField as="textarea" {...inputConfig} style={{borderColor: error && 'red'}} />
+            }
+            default:
+                element = null
+                break;
+        }
+
+        useEffect(() => {
+            console.log('hi')
+        }, [passVisible])
+
     return (
-        <div className={classes.InputWrapper} style={style}>
-            <label>{label}</label>
-            <FastField {...config} style={{borderColor: error && 'red'}}/>
+        <div className={classes.InputWrapper} {...parentConfig}>
+            <label {...labelConfig}>{label}</label>
+            {/* <FastField {...config} style={{borderColor: error && 'red'}}/> */}
+            {element}
             {
                 error &&
                 <p className={classes.DesktopError}>{error}</p>
@@ -17,5 +61,18 @@ export const Input = ({ label, config, style, error}) => {
                 <p className={classes.MobileError}><BiError />&nbsp;{error}</p>
             }
         </div>
+        // <div className={classes.InputWrapper} style={style} {...parentConfig}>
+        //     <label {...labelConfig}>{label}</label>
+        //     <FastField {...config} style={{borderColor: error && 'red'}}/>
+        //     {element}
+        //     {
+        //         error &&
+        //         <p className={classes.DesktopError}>{error}</p>
+        //     }
+        //     {
+        //         error &&
+        //         <p className={classes.MobileError}><BiError />&nbsp;{error}</p>
+        //     }
+        // </div>
     )
 }
