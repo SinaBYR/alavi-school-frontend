745fe366d9ce1c98aac24415d9402b88d5299927
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-08-26 16:47:14 +0430

    Create Reports sub-component

--- src/components/PublicPages/News/News.js
+++ src/components/PublicPages/News/News.js
@@ -1,46 +1,16 @@
 import classes from './News.module.css';
-import image from '../../../assets/kitten.jpg';
-import imageOne from '../../../assets/bird.jpg';
-import imageTwo from '../../../assets/bird-1.jpg';
-import imageThree from '../../../assets/monkey.jpg';
-import { CardTwo } from '../../Utility/UI/Cards/CardTwo/CardTwo';
+
+import { Route, Switch } from 'react-router-dom';
+import { Reports } from './Reports/Reports';
 
 const News = props => {
 
     return (
         <div className={classes.News}>
             <div className={classes.Wrapper}>
-                <h2 style={{textAlign: 'center'}}>اخبار مجموعه</h2>
-                <div className={classes.Content}>
-                    <CardTwo
-                        title="راهپیمایی 22 بهمن"
-                        src={imageOne}
-                        path="/school/news/274698"
-                        views="267"
-                        date="24 بهمن"
-                        />
-                    <CardTwo
-                        title="روز دانش آموز"
-                        src={imageTwo}
-                        path="/school/news/274698"
-                        views="267"
-                        date="4 آبان"
-                        />
-                    <CardTwo
-                        title="برنامه امتحانات نوبت اول"
-                        src={image}
-                        path="/school/news/274698"
-                        views="267"
-                        date="16 اردیبهشت"
-                        />
-                    <CardTwo
-                        title="جلسه دیدار با اولیا"
-                        src={imageThree}
-                        path="/school/news/274698"
-                        views="267"
-                        date="28 فروردین"
-                        />
-                </div>
+               <Switch>
+                   <Route path="/school/news" component={Reports} />
+               </Switch>
             </div>
         </div>
     )
---
+++ src/components/PublicPages/News/Reports/Reports.js
@@ -0,0 +1,46 @@
+import classes from './Reports.module.css';
+import image from '../../../../assets/kitten.jpg';
+import imageOne from '../../../../assets/bird.jpg';
+import imageTwo from '../../../../assets/bird-1.jpg';
+import imageThree from '../../../../assets/monkey.jpg';
+import { CardTwo } from '../../../Utility/UI/Cards/CardTwo/CardTwo';
+
+const Reports = props => {
+    return (
+        <div className={classes.Reports}>
+            <h2 style={{textAlign: 'center'}}>اخبار مجموعه</h2>
+            <div className={classes.ReportsWrapper}>
+                <CardTwo
+                    title="راهپیمایی 22 بهمن"
+                    src={imageOne}
+                    path="/school/news/274698"
+                    views="267"
+                    date="24 بهمن"
+                    />
+                <CardTwo
+                    title="روز دانش آموز"
+                    src={imageTwo}
+                    path="/school/news/274698"
+                    views="267"
+                    date="4 آبان"
+                    />
+                <CardTwo
+                    title="برنامه امتحانات نوبت اول"
+                    src={image}
+                    path="/school/news/274698"
+                    views="267"
+                    date="16 اردیبهشت"
+                    />
+                <CardTwo
+                    title="جلسه دیدار با اولیا"
+                    src={imageThree}
+                    path="/school/news/274698"
+                    views="267"
+                    date="28 فروردین"
+                    />
+            </div>
+        </div>
+    )
+}
+
+export { Reports }
