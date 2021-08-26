84ace5f5cf6fc76107698270eb0d5aaa50edaca8
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-08-26 13:14:04 +0430

    Finish CardTwo component styles in News component

--- src/components/PublicPages/News/News.js
+++ src/components/PublicPages/News/News.js
@@ -1,6 +1,9 @@
 import classes from './News.module.css';
 import image from '../../../assets/kitten.jpg';
 import LinkOne from '../../Utility/UI/LinkOne/LinkOne';
+import { GrView, GrFormView } from 'react-icons/gr';
+import { FiEye } from 'react-icons/fi';
+import { BiCalendarEdit } from 'react-icons/bi';
 
 const News = props => {
     return (
@@ -9,25 +12,133 @@ const News = props => {
                 <h2 style={{textAlign: 'center'}}>اخبار مجموعه</h2>
                 <div className={classes.Content}>
                     <div className={classes.CardTwo}>
-                        <h3>برنامه امتحانات نوبت دوم اصلاحيه</h3>
+                        <h4>برنامه امتحانات نوبت دوم اصلاحيه</h4>
                         <div className={classes.Thumbnail}>
                             <img src={image} alt="kitten"/>
                         </div>
-                        <LinkOne to="school/news/249763" style={{marginRight: 'auto'}}>ادامه مطلب</LinkOne>
+                        <div className={classes.Bottom}>
+                            <div className={classes.Info} style={{display: 'flex', alignItems: 'center'}}>
+                                <div style={{
+                                        // display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                        paddingLeft: '8px',
+                                        textAlign: 'center'
+                                            }}>
+                                    <BiCalendarEdit />
+                                    <div>16 اردیبهشت</div>
+                                </div>
+                                <div style={{
+                                        // display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                        paddingRight: '8px',
+                                        // borderRight: '1px solid #dbdbdb',
+                                        textAlign: 'center'
+                                            }}>
+                                    <FiEye />
+                                    <div>299</div>
+                                </div>
+                            </div>
+                            <LinkOne to="school/news/249763" style={{marginRight: 'auto'}}>ادامه مطلب</LinkOne>
+                        </div>
                     </div>
                     <div className={classes.CardTwo}>
-                        <h3>برنامه امتحانات نوبت دوم اصلاحيه</h3>
+                        <h4>برنامه امتحانات نوبت دوم اصلاحيه</h4>
                         <div className={classes.Thumbnail}>
                             <img src={image} alt="kitten"/>
                         </div>
-                        <LinkOne to="school/news/987234" style={{marginRight: 'auto'}}>ادامه مطلب</LinkOne>
+                        <div className={classes.Bottom}>
+                            <div className={classes.Info} style={{display: 'flex', alignItems: 'center'}}>
+                                <div style={{
+                                        // display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                        paddingLeft: '8px',
+                                        textAlign: 'center'
+                                            }}>
+                                    <BiCalendarEdit />
+                                    <div>16 اردیبهشت</div>
+                                </div>
+                                <div style={{
+                                        // display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                        paddingRight: '8px',
+                                        // borderRight: '1px solid #dbdbdb',
+                                        textAlign: 'center'
+                                            }}>
+                                    <FiEye />
+                                    <div>299</div>
+                                </div>
+                            </div>
+                            <LinkOne to="school/news/249763" style={{marginRight: 'auto'}}>ادامه مطلب</LinkOne>
+                        </div>
                     </div>
                     <div className={classes.CardTwo}>
-                        <h3>راهپیمایی 22 بهمن</h3>
+                        <h4 style={{textAlign: 'center'}}>برنامه امتحانات نوبت دوم اصلاحيه</h4>
                         <div className={classes.Thumbnail}>
                             <img src={image} alt="kitten"/>
                         </div>
-                        <LinkOne to="school/news/139843" style={{marginRight: 'auto'}}>ادامه مطلب</LinkOne>
+                        <div className={classes.Bottom}>
+                            <div className={classes.Info} style={{display: 'flex', alignItems: 'center'}}>
+                                <div style={{
+                                        fontSize: '12px',
+                                        paddingLeft: '8px',
+                                        textAlign: 'center'
+                                        }}>
+                                    <BiCalendarEdit />
+                                    <div>16 اردیبهشت</div>
+                                </div>
+                                <div style={{
+                                        fontSize: '12px',
+                                        paddingRight: '8px',
+                                        textAlign: 'center'
+                                        }}>
+                                    <FiEye />
+                                    <div>299</div>
+                                </div>
+                            </div>
+                            <LinkOne to="school/news/249763" style={{marginRight: 'auto'}}>ادامه مطلب</LinkOne>
+                        </div>
+                    </div>
+                    <div className={classes.CardTwo}>
+                        <h4>برنامه امتحانات نوبت دوم اصلاحيه</h4>
+                        <div className={classes.Thumbnail}>
+                            <img src={image} alt="kitten"/>
+                        </div>
+                        <div className={classes.Bottom}>
+                            <div className={classes.Info} style={{display: 'flex', alignItems: 'center'}}>
+                                <div style={{
+                                        // display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                        paddingLeft: '8px',
+                                        textAlign: 'center'
+                                            }}>
+                                    <BiCalendarEdit />
+                                    <div>16 اردیبهشت</div>
+                                </div>
+                                <div style={{
+                                        // display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        fontSize: '12px',
+                                        paddingRight: '8px',
+                                        // borderRight: '1px solid #dbdbdb',
+                                        textAlign: 'center'
+                                            }}>
+                                    <FiEye />
+                                    <div>299</div>
+                                </div>
+                            </div>
+                            <LinkOne to="school/news/249763" style={{marginRight: 'auto'}}>ادامه مطلب</LinkOne>
+                        </div>
                     </div>
                 </div>
             </div>
