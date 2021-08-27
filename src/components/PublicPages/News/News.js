82f5c7c9d944c4998540fe95f03463fb89f17b1f
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-08-27 08:15:26 +0430

    Finish Report sub-component styles

--- src/components/PublicPages/News/News.js
+++ src/components/PublicPages/News/News.js
@@ -2,6 +2,7 @@ import classes from './News.module.css';
 
 import { Route, Switch } from 'react-router-dom';
 import { Reports } from './Reports/Reports';
+import { Report } from './Report/Report';
 
 const News = props => {
 
@@ -9,6 +10,7 @@ const News = props => {
         <div className={classes.News}>
             <div className={classes.Wrapper}>
                <Switch>
+                   <Route path="/school/news/:reportID" component={Report}/>
                    <Route path="/school/news" component={Reports} />
                </Switch>
             </div>
---
+++ src/components/PublicPages/News/Report/Report.js
@@ -0,0 +1,40 @@
+import classes from './Report.module.css';
+import image from '../../../../assets/kitten.jpg';
+import { BiCalendarEdit } from 'react-icons/bi';
+import { FiEye } from 'react-icons/fi';
+import HTwo from '../../../Utility/UI/Headings/HTwo/HTwo'
+
+const Report = props => {
+    return (
+        <div className={classes.Report}>
+            <HTwo style={{textAlign: 'center'}}>راهپیمایی 22 بهمن</HTwo>
+            <div className={classes.Header}>
+                <div style={{
+                    textAlign: 'center'
+                }}>
+                    <BiCalendarEdit />
+                    <div>16 اسفند 1398</div>
+                </div>
+
+                <div style={{
+                    textAlign: 'center'
+                }}>
+                    <FiEye />
+                    <div>274</div>
+                </div>
+            </div>
+            <div className={classes.Content}>
+                <div className={classes.Image}>
+                    <img src={image} alt="kitten" />
+                </div>
+                <div className={classes.Text}>
+                    <p>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری  چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبدرا برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { Report }
