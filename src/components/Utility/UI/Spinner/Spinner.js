323fb6d20f9ce04a55064abec396e355e2d63ba7
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-08-29 08:55:10 +0430

    Add Spinner UI component

---
+++ src/components/Utility/UI/Spinner/Spinner.js
@@ -0,0 +1,7 @@
+import classes from './Spinner.module.css';
+
+export const Spinner = props => {
+    return (
+        <div className={classes.Spinner}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+}
